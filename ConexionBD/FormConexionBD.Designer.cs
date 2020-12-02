9972d4fac22ccc57b53e596582b3edd3b1b1159d
ChristianBautista3108 <bautistalealchristianjohel@gmail.com>
2020-12-01 20:46:48 -0500

    Agregar archivos de proyecto.

---
+++ ConexionBD/FormConexionBD.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ConexionBD
+{
+    partial class FormConexionBD
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConexionBD));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EstadoInicial = new System.Windows.Forms.Label();
+            this.EstadoFinal = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+            this.panel1.Controls.Add(this.EstadoFinal);
+            this.panel1.Controls.Add(this.EstadoInicial);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 207);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(14, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Estados de conexion de la BD";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.Location = new System.Drawing.Point(57, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mostrar Estado";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(8, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(63, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Estado inicial:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(63, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Estado final:";
+            // 
+            // EstadoInicial
+            // 
+            this.EstadoInicial.AutoSize = true;
+            this.EstadoInicial.ForeColor = System.Drawing.SystemColors.Control;
+            this.EstadoInicial.Location = new System.Drawing.Point(143, 88);
+            this.EstadoInicial.Name = "EstadoInicial";
+            this.EstadoInicial.Size = new System.Drawing.Size(23, 13);
+            this.EstadoInicial.TabIndex = 5;
+            this.EstadoInicial.Text = "null";
+            // 
+            // EstadoFinal
+            // 
+            this.EstadoFinal.AutoSize = true;
+            this.EstadoFinal.ForeColor = System.Drawing.SystemColors.Control;
+            this.EstadoFinal.Location = new System.Drawing.Point(143, 119);
+            this.EstadoFinal.Name = "EstadoFinal";
+            this.EstadoFinal.Size = new System.Drawing.Size(23, 13);
+            this.EstadoFinal.TabIndex = 6;
+            this.EstadoFinal.Text = "null";
+            // 
+            // FormConexionBD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 206);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormConexionBD";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label EstadoFinal;
+        private System.Windows.Forms.Label EstadoInicial;
+    }
+}
+
---
+++ ConexionBD/FormConexionBD.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexionBD
+{
+    public partial class FormConexionBD : Form
+    {
+        public FormConexionBD()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConexionBase();
+        }
+
+         public void ConexionBase()
+        {
+            string stringConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\chris\\source\\repos\\ConexionBD\\ConexionBD\\PruebaConexion.mdf;Integrated Security=True";
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = stringConexion;
+
+            try
+            {
+                connection.Open();
+                EstadoInicial.Text = "Conectado";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("error" + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+                EstadoFinal.Text = "Desconectado";
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
